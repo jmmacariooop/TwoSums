16a4f73ec389ba1229f55b0a312366921e3d7f8f
JUSTINE MAE MACARIO <jbm1737@students.uc-bcf.edu.ph>
2024-03-08 18:37:29 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿public class TwoSums
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dictionary = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (dictionary.ContainsKey(complement))
+            {
+                return new int[] { dictionary[complement], i };
+            }
+            dictionary[nums[i]] = i;
+        }
+        throw new Exception("No two sum solution");
+    }
+}
